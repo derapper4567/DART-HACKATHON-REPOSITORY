3396451b610adc1fe49d98c0e4426f5859f8d232
asha <ashakisonga2022@gmail.com>
2024-03-18 23:19:41 +0300

    dart programming

--- functions.dart
+++ functions.dart
@@ -1,2 +1,21 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+/* Program 2: Perform Mathematical Operations with Functions
+Write a Dart program that performs two mathematical operations using functions.*/
+
+void main() {
+  int num1 = 3;
+  int num2 = 5;
+
+  int sum = add(num1, num2);
+  print("Sum of $num1 and $num2 is: $sum");
+
+  int product = multiply(num1, num2);
+  print("Product of $num1 and $num2 is: $product");
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,11 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+/* Program 1: Display Personal Information
+Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.*/
+
+void main() {
+  String name = "Asha";
+  int age = 22;
+  String school = "University of Dar es Salaam,Tanzania";
+  String hobby = "Listening music and coding";
+
+  print("$name is $age years old, studying at $school, and enjoys $hobby.");
+}
